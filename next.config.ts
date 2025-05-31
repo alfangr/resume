866f641137e9960a55510bd620abb5cdfbb760bc
Alfan Ghinan Rusydi <alfangr05@gmail.com>
2025-05-31 20:28:06 +0700

    [feat] update config

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
+  basePath: "/alfangr.dev",
+  assetPrefix: "/alfangr.dev",
+  distDir: "dir",
 };
 
 export default nextConfig;
