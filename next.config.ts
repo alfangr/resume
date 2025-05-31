eda68512121ea66d6d8dc9574a8b548bfd5ec2e7
Alfan Ghinan Rusydi <alfangr05@gmail.com>
2025-05-31 20:45:17 +0700

    [fix] add path

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: "",
-  assetPrefix: "",
+  basePath: "/alfangr.dev",
+  assetPrefix: "/alfangr.dev/",
   distDir: "dir",
   trailingSlash: true,
   images: {
