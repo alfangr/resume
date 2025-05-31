dee45b6cb942dbbe5152f498100e162c8b4f916b
Alfan Ghinan Rusydi <alfangr05@gmail.com>
2025-05-31 20:51:01 +0700

    [feat] back to our dir

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,6 @@ const nextConfig: NextConfig = {
   output: "export",
   basePath: "/alfangr.dev",
   assetPrefix: "/alfangr.dev/",
-  distDir: "dir",
   trailingSlash: true,
   images: {
     unoptimized: true,
